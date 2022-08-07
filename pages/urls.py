3dd5ffdbbda804205bb6381a1ae74ecdd9ec0cc2
Erdrin Kaja <erdrinkaja.ek@gmail.com>
2022-08-07 12:13:49 +0200

    fallxhoret

--- pages/urls.py
+++ pages/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.HomePage.as_view(), name='home page'),
-    path('about/', views.AboutPage.as_view(), name='about page'),
+    path('fortune-teller/', views.FortuneTeller.as_view(), name='fortune teller'),
     path('contact/', views.ContactPage.as_view(), name='contact page'),
     path('events/', views.Event.as_view(), name='event page'),
 
--- pages/views.py
+++ pages/views.py
@@ -6,8 +6,8 @@ class HomePage(TemplateView):
     template_name = 'pages/home_page.html'
 
 
-class AboutPage(TemplateView):
-    template_name = 'pages/about_page.html'
+class FortuneTeller(TemplateView):
+    template_name = 'pages/fortune_teller.html'
 
 
 class ContactPage(TemplateView):
