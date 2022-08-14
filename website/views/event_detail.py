339b61f0dec57c3e282f8aa2c19809d1fd69bd80
Erdrin Kaja <erdrinkaja.ek@gmail.com>
2022-08-14 14:50:08 +0200

    I perfunduar

--- website/views/event_detail.py
+++ website/views/event_detail.py
@@ -10,18 +10,24 @@ def event_detail(request, id):
 
     moon = Moon.objects.filter(valid_date=datetime.now().date())[0]
     phase_id = moon.phase_id
+    moon_id = moon.id
     status_id = moon.status
     phase = Phase.objects.filter(pk=phase_id)[0]
     event = Event.objects.filter(pk=id)[0]
     moon_img_url = phase.image.url
 
+    for mid in range(moon_id, moon_id + 20):
+        if Moon.objects.get(id=mid).status == event.status:
+            date = Moon.objects.get(id=mid).valid_date
+            break
+
     suggestion = None
     if status_id == 3:
         suggestion = "Careful!!!, Today it is not suggested to make any importand decision because it is a blood moon"
     if status_id == event.status:
-        suggestion = "Today, the position of the moon is in your favor. \n If you would like to consider a different date, click the above button."
+        suggestion = "Today, the position of the moon is in your favor. \n If you would like to consider a different date, click the berlow button."
     else:
-        suggestion = "Today, the position of the moon is not in your favor"
+        suggestion = f"Today, the position of the moon is not in your favor. Please wait until date: {date} than. \n If you would like to consider a different date, click the below button."
 
 
     return render(request, 'pages/event-page-detail.html', context={'phase': phase, 'img_url': moon_img_url, 'event': event, 'suggestion': suggestion})
@@ -30,19 +36,24 @@ def event_detail(request, id):
 def event_detail_for_date(request, id):
     check_date = request.POST.get('checkDate', None)
     moon = Moon.objects.filter(valid_date=check_date)[0]
+    moon_id = moon.id
     phase_id = moon.phase_id
     status_id = moon.status
     phase = Phase.objects.filter(pk=phase_id)[0]
     event = Event.objects.filter(pk=id)[0]
     moon_img_url = phase.image.url
 
+    for mid in range(moon_id, moon_id + 20):
+        if Moon.objects.get(id=mid).status == event.status:
+            date = Moon.objects.get(id=mid).valid_date
+            break
+
     suggestion = None
     if status_id == 3:
         suggestion = "Careful!!!, This it is not suggested to make any importand decision because it is a blood moon"
     if status_id == event.status:
-        suggestion = "This day, the position of the moon is in your favor. \n If you would like to consider a different date, click the above button."
+        suggestion = "This day, the position of the moon is in your favor. \n If you would like to consider a different date, click the below button."
     else:
-        suggestion = "This day, the position of the moon is not in your favor. \n If you would like to consider a different date, click the above button."
-
+        suggestion = f"This day, the position of the moon is not in your favor. Please wait until date: {date} than. \n If you would like to consider a different date, click the below button."
     return render(request, 'pages/event-page-detail.html',
                   context={'phase': phase ,'img_url': moon_img_url, 'event': event, 'suggestion': suggestion})
--- website/views/page_view.py
+++ website/views/page_view.py
@@ -1,4 +1,9 @@
+import random
+
+from django.db.models import Q
 from django.shortcuts import render
+
+from entities.models.event import Event
 from entities.models.phase import Phase
 from entities.models.moon import Moon
 from datetime import datetime
@@ -7,9 +12,14 @@ from datetime import datetime
 def home_page(request):
     moon = Moon.objects.filter(valid_date=datetime.now().date())[0]
     phase_id = moon.phase_id
+    st = moon.status
     phase = Phase.objects.filter(pk=phase_id)[0]
     moon_img_url = phase.image.url
     title = phase.title
+    event = Event.objects.filter(status=st)
+    evn_in_favor = random.choice(event)
+    even = Event.objects.filter(~Q(status=st))
+    evn_in_disfavor = random.choice(even)
 
     if moon.phase_day == 1:
         description = phase.first_description
@@ -18,4 +28,4 @@ def home_page(request):
     else:
         description = phase.third_description
 
-    return render(request, 'pages/home-page.html', context={'img_url': moon_img_url, 'description': description, 'title': title })
+    return render(request, 'pages/home-page.html', context={'img_url': moon_img_url, 'description': description, 'title': title, 'ev_in_favor': evn_in_favor, 'ev_in_disfavor': evn_in_disfavor })
